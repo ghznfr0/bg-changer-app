a8cecd5fd4d78c7562c472118b75baff10fb01a8
Ghazanfar <ghznfr.uos@gmail.com>
2024-12-13 14:01:25 +0500

    my first git app

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addValue = () => {
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+  }
+  const removeVal = () => {
+    if(count > 0){
+      setCount(count -1)
+    }
+  }
+
+  return (
+    <>
+      <button onClick={addValue}>Add</button>
+      <h2>Counter: {count} </h2>
+      <button onClick={removeVal}>Remove</button>
+    </>
+  )
+}
+
+export default App
